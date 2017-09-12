d8d97c23e67e63a595a66efffa2e111b6ce5fd75
Fanny10 <fanny.lindqvist@sundbyberg.it-gymnasiet.se>
2017-09-12 11:11:43 +0200

    lektion 4

---
+++ src/lektion4/oop/CarGame.java
@@ -0,0 +1,18 @@
+package lektion4.oop;
+
+public class CarGame {
+
+	public static void main(String[] args) {
+		Car fannysBil = new Car ();
+		
+		
+		System.out.println(fannysBil.getSpeed());
+		
+		fannysBil.setSpeed(2000);
+		System.out.println(fannysBil.getSpeed());
+	
+		
+
+	}
+
+}
