3bdd4720d94f2fa515b82e68e5d7131b699c067a
Fanny10 <fanny.lindqvist@sundbyberg.it-gymnasiet.se>
2017-09-26 11:40:20 +0200

    lektion 6

---
+++ src/lesson6/exercises/Application.java
@@ -0,0 +1,15 @@
+package lesson6.exercises;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word");
+		// VI har skapat et object av klassen Person
+		Person fannysPerson = new Person("Fanny");
+		fannysPerson.writeName();
+		
+		fannysPerson.setName("Mow");
+		fannysPerson.writeName();
+	}
+
+}
