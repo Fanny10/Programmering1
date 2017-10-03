ef8e6b3ec455506ff66ba1db7975a51c93f1cec8
Fanny10 <fanny.lindqvist@sundbyberg.it-gymnasiet.se>
2017-10-03 11:41:46 +0200

    :)

---
+++ src/exercises/oop/Employee.java
@@ -0,0 +1,61 @@
+package exercises.oop;
+
+public class Employee {
+	private int id() {
+
+	}
+
+	private String firstName {
+
+	}
+
+	private String lastName {
+
+	}
+
+	private int salary() {
+
+	}
+
+	public Employee(int id, String firstname, String lastName, int salary) {
+
+	}
+
+	public int getId() {
+
+	}
+
+	public String getFirstName("Fanny") {
+
+	}
+
+	public String getLastName("Lindqvist") {
+		
+	}
+
+	public String getName("Fanny Lindqvist") {
+		
+	}
+
+	public String getSalary() {
+
+	}
+
+	public int getSalary() {
+
+	}
+
+	public void setSalary(int salary) {
+
+	}
+
+	public int getAnnualSalary(salary * 12) {
+		
+	}
+
+	public int raiseSalary(int percent) {
+
+	}
+
+	public String toString() {
+}
