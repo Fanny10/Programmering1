acc62bb33152382d4ff855d55ce17bf91569ccd2
Fanny10 <fanny.lindqvist@sundbyberg.it-gymnasiet.se>
2017-11-15 11:21:21 +0100

    2017.11.15

--- src/ifstatements/exercises/CodeNumber.java
+++ src/ifstatements/exercises/CodeNumber.java
@@ -1,6 +1,6 @@
 package ifstatements.exercises;
 
-
+import java.util.Scanner;
 
 /*Write a Java program to determine whether an input number is an even number.
 * Represent it with code:
@@ -10,15 +10,18 @@ public class CodeNumber {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		
-		
-		
+		Scanner userGuess = new Scanner(System.in);
+		System.out.println("Välj ett tal :");
 
-	
+		int tal = userGuess.nextInt();
+
+		if (tal % 2 == 1) {
+			System.out.println("Udda");
+
+		} else {
+			System.out.println("Jämna");
+		}
+
 	}
 
-
-	}
-
+}
