1c0d516b84108c7849f97176ed077064cf7f336f
Fanny10 <fanny.lindqvist@sundbyberg.it-gymnasiet.se>
2017-12-14 11:58:11 +0100

    2017.12.14

--- src/spel/Spel.java
+++ src/spel/Spel.java
@@ -9,6 +9,8 @@ import com.sun.xml.internal.ws.api.config.management.policy.ManagementAssertion.
 
 public class Spel {
 	private static int liv = 10;
+	private static int vapen = 0;
+	private static int nyckel = 0;
 
 	public static void main(String[] args) {
 		start();
@@ -38,12 +40,17 @@ public class Spel {
 
 	public static void level1() {
 		JOptionPane.showMessageDialog(null, "Level 1");
+		början();
+	}
+
+	public static void början() {
 
 		Object[] options = new Object[] { "Tar en annan väg", "Låsas som du inte ser och går förbi",
 				"Frågar om personen behöver hjälp" };
+		ImageIcon icon = new ImageIcon("C:/Users/Fanny.Lindqvist/Desktop/dagväg.jpg");
 		int val = JOptionPane.showOptionDialog(null,
 				"Du är påväg hem och du ser någon som ligger på marken " + " \n Vad gör du?", null,
-				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options, options[2]);
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, icon, options, options[2]);
 
 		if (val == 0) {
 
@@ -77,11 +84,92 @@ public class Spel {
 
 		}
 	}
+
+	public static void tillbaksEfterSkogen() {
+		Object[] options1 = { "På den vägen människan var", "Genom ett hus område", "Vägen bakom" };
+
+		int enAnnan = JOptionPane.showOptionDialog(null, "Okej, du valde en annan väg " + " \n Vilken?", null,
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options1, options1[2]);
+
+		if (enAnnan == 0) {
+			början();
+
+		} else if (enAnnan == 1) {
+			hus();
+
+		} else {
+			vägenTillbaks();
+
+		}
+	}
+
 	public static void vägenTillbaks() {
+		Object[] options7 = { "Väntar på bussen", "ringer en taxi", "Går in i någonstans" };
+
+		int tillbaks = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att gå på vägen bakom \n Du kommer till stan där du var förut och men stan var väldigt öde \n det finns en buss som går om en timme"
+						+ " \n Vad ska du göra?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options7, options7[2]);
+		if (tillbaks == 0) {
+			bussen();
+		} else if (tillbaks == 1) {
+			taxi();
+		} else {
+			affär();
+		}
+	}
+
+	public static void bussen() {
+		Object[] options7 = { "Väntar på bussen", "ringer en taxi", "Går in i någon affär" };
+
+		int buss = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att vänta på bussen \n men det kommer två personer gåendes mot parkeringen"
+						+ " \n Vad ska du göra?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options7, options7[2]);
+
+		if (buss == 0) {
+
+		} else if (buss == 1) {
+
+		} else {
+
+		}
+	}
+
+	public static void taxi() {
 		Object[] options7 = { "", "", "" };
 
-		int hus = JOptionPane.showOptionDialog(null, "Okej, du valde att gå på vägen bakom \n Du kommer till stan där du var förut och" + " \n Vad gör du ?",
+		int taxi = JOptionPane.showOptionDialog(null, "Okej, du valde att ringa en taxi \n " + " \n Vad ska du göra?",
 				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options7, options7[2]);
+
+		if (taxi == 0) {
+
+		} else if (taxi == 1) {
+
+		} else {
+
+		}
+	}
+
+	public static void affär() {
+		Object[] options7 = { "Väntar på bussen", "ICA", "" };
+
+		int affär = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att gå in i någonstans \n Men det är bara två som är öppna"
+						+ " \n Vilken ska du ska gå in i eller ska du vänta?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options7, options7[2]);
+
+		if (affär == 0) {
+			bussen();
+		} else if (affär == 1) {
+			ica();
+		} else {
+
+		}
+	}
+
+	public static void ica() {
+
 	}
 
 	public static void gåFörbi() {
@@ -94,28 +182,67 @@ public class Spel {
 			ljudPåVägen();
 		} else if (person == 1) {
 			storaHuset();
-			
 
-		}else {
-			
+		} else {
+			help();
+
 		}
 	}
+
 	public static void storaHuset() {
-		Object[] options7 = { "knackar på", "fortsätter gå", "går tillbaks till personen" };
+		Object[] options7 = { "knackar på", "fortsätter gå", "går tillbaks och hjälper personen" };
 
 		int vägen = JOptionPane.showOptionDialog(null,
-				"Okej, du valde att säga till \n Men vägen är tom men du ser ett stort hus framför dig" + " \n Vad gör du ?", null,
-				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options7, options7[2]);
-		if(vägen == 0) {
+				"Okej, du valde att säga till \n Men vägen är tom men du ser ett stort hus framför dig"
+						+ " \n Vad gör du ?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options7, options7[2]);
+		if (vägen == 0) {
 			knackar();
-			
-		}else if(vägen == 1) {
+
+		} else if (vägen == 1) {
 			ljudPåVägen();
-		}else {
-			
+		} else {
+			help();
 		}
 	}
+
 	public static void knackar() {
+		Object[] options3 = { "Säger hallå, någon hemma?", " Smygger in och tar ficklampan", "" };
+
+		int help = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att knacka på \n Dörren öppnas lite på glänt och du får syn på en fiklampa längst in i korridoren "
+						+ " \n Vad gör du?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options3, options3[2]);
+		if (help == 0) {
+			sägerHallå();
+
+		} else if (help == 1) {
+			fickLampa();
+
+		} else {
+
+		}
+
+	}
+	public static void sägerHallå() {
+		
+	}
+	public static void fickLampa(){
+		Object[] options3 = { "Säger hallå, någon hemma?", " Smygger in och tar fiklampan", "" };
+
+		int help = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att smygga in \n men du kom inte långt"
+						+ " \n Vad gör du?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options3, options3[2]);
+		if (help == 0) {
+			
+
+		} else if (help == 1) {
+			
+
+		} else {
+
+		}
 		
 	}
 
@@ -142,52 +269,52 @@ public class Spel {
 
 	// level2();
 	public static void skog() {
-		while (true) {
 
-			Object[] options5 = { "Går igenom skogen i alla fall", "Genom ett hus område",
-					"Går tillbaka till vägen bakom" };
-			int skog = JOptionPane.showOptionDialog(null,
-					"Okej, du valde att gå igenom skogen \n Men du hör konstiga ljud framför dig" + " \n Vad gör du ?",
-					null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
+		Object[] options5 = { "Går igenom skogen i alla fall", "Genom ett hus område",
+				"Går tillbaka till vägen bakom" };
+		int skog = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att gå igenom skogen \n Men du hör konstiga ljud framför dig" + " \n Vad gör du ?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
 
-			if (skog == 0) {
-				liv = liv - 1;
+		if (skog == 0) {
+			liv = liv - 1;
+			ImageIcon icon = new ImageIcon("C:/Users/Fanny.Lindqvist/Desktop/björn.jpg");
+			JOptionPane.showMessageDialog(null,
+					"Okej, du valde att gå i skogen i alla fall \n Det var en björn, och du har nu -1 liv"
+							+ " \n du har liv kvar " + liv, null, skog, icon);
+			skog();
 
-				JOptionPane.showMessageDialog(null,
-						"Okej, du valde att gå i skogen i alla fall \n Det var en björn, och du har nu -1 liv"
-								+ " \n du har liv kvar " + liv);
+		} else if (skog == 1) {
+			hus();
 
-			} else if (skog == 1) {
-				hus();
-				break;
-			} else {
-				enAnnan();
-
-			}
+		} else {
+			tillbaksEfterSkogen();
 
 		}
 
 	}
+
 	public static void ljudPåVägen() {
 		Object[] options5 = { "Går igenom skogen", "fortsätter gå", " går tillbaks till människan som behöver hjälp" };
 
 		int ut = JOptionPane.showOptionDialog(null,
-				"Okej, du valde att fortsätta gå \n Men du hör konstiga ljud framför dig" + " \n Vad gör du ?",
-				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
-		if(ut == 0) {
+				"Okej, du valde att fortsätta gå \n Men du hör konstiga ljud framför dig" + " \n Vad gör du ?", null,
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
+		if (ut == 0) {
 			storaSkogen();
-		}else if(ut == 1) {
+		} else if (ut == 1) {
 			fortsätterGå();
-		}else {
-			
+		} else {
+
 		}
 	}
+
 	public static void fortsätterGå() {
-		
-		
+
 	}
+
 	public static void storaSkogen() {
-		
+
 	}
 
 	public static void hus() {
@@ -202,6 +329,7 @@ public class Spel {
 			husfolk();
 
 		} else {
+			enAnnan();
 
 		}
 	}
@@ -215,11 +343,106 @@ public class Spel {
 						+ " \n Vad gör du?",
 				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options1, options1[2]);
 		if (ambulans == 0) {
-
+			lämnarVapnet();
 		} else if (ambulans == 1) {
-
+			TarVapnet();
 		} else {
 			polisen1();
+		}
+	}
+
+	public static void TarVapnet() {
+		Object[] options1 = { "Skyndar sig raka vägen hem", "Tar en genväg igenom skogen", "Ringer en taxi" };
+
+		int ambulans = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att ta med vapnet \n Det börjar bli riktigt mörkt ute " + " \n Vad gör du?", null,
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options1, options1[2]);
+
+		if (ambulans == 0) {
+			AndraVägenTillHuset();
+
+		} else if (ambulans == 1) {
+			mörkaSkogen();
+
+		} else {
+			taxi();
+		}
+	}
+
+	public static void lämnarVapnet() {
+		Object[] options1 = { "Skyndar sig raka vägen hem", "Tar en genväg igenom skogen", "Ringer en taxi" };
+
+		int ambulans = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att lämna vapnet \n Det börjar bli riktigt mörkt ute " + " \n Vad gör du?", null,
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options1, options1[2]);
+		if (ambulans == 0) {
+			AndraVägenTillHuset();
+
+		} else if (ambulans == 1) {
+			mörkaSkogen();
+
+		} else {
+			taxi();
+		}
+
+	}
+
+	public static void mörkaSkogen() {
+
+		Object[] options5 = { "Går igenom skogen i alla fall", "", "Gå till Storahuset" };
+		int skog = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att gå igenom skogen \n Men du hör konstiga ljud framför dig" + " \n Vad gör du ?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
+
+		if (skog == 0) {
+			skogsBjörn();
+
+		} else if (skog == 1) {
+			förbiHusetPåNatten();
+
+		} else {
+			storaHuset();
+		}
+	}
+
+	public static void skogsBjörn() {
+		liv = liv - 1;
+		ImageIcon icon = new ImageIcon("C:/Users/Fanny.Lindqvist/Desktop/björn.jpg");
+		JOptionPane.showInternalMessageDialog(null,
+				"Okej, du valde att gå i skogen i alla fall \n Det var en björn, och du har nu -1 liv "
+						+ " \n du har liv kvar " + liv, null, liv, icon);
+		mörkaSkogen();
+	}
+
+	public static void AndraVägenTillHuset() {
+
+		Object[] options2 = { "försöker gå i mörkret", "Knackar på", "går igenom hus området längre bort" };
+		int vägHus = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att fortsätta gå \n Du kommer fram till ett stort hus, din mobil har dött och det är en väldigt mörkt ute "
+						+ " \n Du skulle behöva en fiklampa för att hitta hem \n Vad gör du?",
+				null, JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options2, options2[2]);
+		if (vägHus == 0) {
+
+		} else if (vägHus == 1) {
+			knackar();
+		} else {
+			hus();
+
+		}
+	}
+
+	public static void förbiHusetPåNatten() {
+		Object[] options5 = { "Går igenom skogen", "fortsätter gå", " går tillbaks till huset" };
+
+		int ut = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att fortsätta gå \n Men du hör konstiga ljud framför dig" + " \n Vad gör du ?", null,
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
+		if (ut == 0) {
+			storaSkogen();
+		} else if (ut == 1) {
+			fortsätterGå();
+		} else {
+
 		}
 	}
 
@@ -247,6 +470,21 @@ public class Spel {
 		} else if (personen == 1) {
 			rädd();
 		} else {
+			gårTillSjukhuset();
+		}
+	}
+
+	public static void gårTillSjukhuset() {
+		Object[] options5 = { "", "", "" };
+
+		int ut = JOptionPane.showOptionDialog(null,
+				"Okej, du valde att fortsätta gå till sjukhuset \n " + " \n Vad gör du ?", null,
+				JOptionPane.YES_NO_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE, null, options5, options5[2]);
+		if (ut == 0) {
+
+		} else if (ut == 1) {
+
+		} else {
 
 		}
 	}
@@ -266,6 +504,7 @@ public class Spel {
 						+ " \n Du får -1 liv" + " så du har nu " + liv + "kvar");
 		personen();
 	}
+
 	public static void hemma() {
 		level2();
 	}
